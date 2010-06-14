096e290a34dd4c66bee714ed751ee78d14830cdd
Tlousky <tlousky@gmail.com>
2010-06-14 14:57:03 +0300

    Adding module files

---
+++ lib/Algorithm/Shape/RandomTree.pm
@@ -0,0 +1,110 @@
+package Algorithm::Shape::RandomTree;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Algorithm::Shape::RandomTree - The great new Algorithm::Shape::RandomTree!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Algorithm::Shape::RandomTree;
+
+    my $foo = Algorithm::Shape::RandomTree->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Tamir Lousky, C<< <tlousky at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-algorithm-shape-randomtree at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Algorithm-Shape-RandomTree>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Algorithm::Shape::RandomTree
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Algorithm-Shape-RandomTree>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Algorithm-Shape-RandomTree>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Algorithm-Shape-RandomTree>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Algorithm-Shape-RandomTree/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Tamir Lousky.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Algorithm::Shape::RandomTree
